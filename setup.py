bf7d017f3bea296455670a35494b68442f95d3cd
Eliseu Gaspar Gonçalves - Eilistar <97483099+EliseuGaspar@users.noreply.github.com>
2023-04-20 00:45:12 +0100

    mudança na estrutura do projeto

--- setup.py
+++ setup.py
@@ -1,1 +1,28 @@
-setup.py
+from setuptools import setup
+
+setup(
+    name = 'herojson',
+    version = '1.0',
+    author = 'Eliseu Gaspar',
+    author_email = 'eliseugaspar4@gmail.com',
+    packages = ['herojson'],
+    description = 'um modúlo python criado para facilitar a manipulação de arquivos json',
+    long_description = 'Um simples modúlo python criado para facilitar a manipulação de arquivos json. \
+    Com funções de adicionar, apagar, atualizar e pegar',
+    url = 'https://github.com/EliseuGaspar/herojson',
+    project_urls = {
+        'Código fonte': 'https://github.com/EliseuGaspar/herojson',
+        'Download': 'https://github.com/EliseuGaspar/herojson.git'
+    },
+    license = 'MIT',
+    keywords = ['modúlo','json','hero'],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Portuguese (Angola)',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Scientific/Engineering :: Physics'
+    ]
+)
